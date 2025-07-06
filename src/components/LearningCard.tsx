5166ca0a01eb589d9f3a32d25a31f05034cb8e33
Martin Klejewski <booleanbill@gmail.com>
2025-07-06 08:09:56 +0200

    styling

--- src/components/LearningCard.tsx
+++ src/components/LearningCard.tsx
@@ -19,22 +19,22 @@ export default function LearningCard({ question, answer, questionNumber, topic }
         <div className="relative w-full max-w-2xl aspect-video cursor-pointer perspective-1000" onClick={handleFlip}>
             <div className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d ${isFlipped ? 'rotate-y-180' : ''}`}>
                 {/* Front side */}
-                <div className="absolute inset-0 w-full h-full bg-base-100 rounded-lg shadow-md p-4 backface-hidden">
+                <div className="absolute inset-0 w-full h-full bg-base-100 rounded-lg shadow-md py-6 px-4 backface-hidden">
                     <div className="h-full flex flex-col justify-center items-center text-center">
-                        <div className="absolute top-2 right-2 text-xs text-base-content">{topic} #{questionNumber}</div>
+                        <div className="absolute top-2 right-2 text-xs text-info">{topic} #{questionNumber}</div>
                         <h2 className="text-xl font-semibold mb-4 text-base-content">Frage</h2>
-                        <p className="text-base-content overflow-y-auto px-4 mb-4">{question}</p>
-                        <div className="text-xs text-base-content text-right mt-auto">Klicken für Antwort</div>
+                        <p className="text-base-content overflow-y-auto px-4 mb-auto">{question}</p>
+                        <div className="absolute bottom-2 left-0 w-full text-center text-xs text-info">Klicken für Antwort</div>
                     </div>
                 </div>
 
                 {/* Back side */}
                 <div className="absolute inset-0 w-full h-full bg-base-100 rounded-lg shadow-md p-4 backface-hidden rotate-y-180">
                     <div className="h-full flex flex-col justify-center items-center text-center">
-                        <div className="absolute top-2 right-2 text-sm text-base-content">{topic} #{questionNumber}</div>
+                        <div className="absolute top-2 right-2 text-xs text-info">{topic} #{questionNumber}</div>
                         <h2 className="text-xl font-semibold mb-4 text-base-content">Antwort</h2>
-                        <p className="text-base-content overflow-y-auto px-4 mb-4">{answer}</p>
-                        <div className="text-xs text-base-content text-right mt-auto">Klicken für Frage</div>
+                        <p className="text-base-content overflow-y-auto px-4 mb-auto">{answer}</p>
+                        <div className="absolute bottom-3 left-0 w-full text-center text-xs text-info">Klicken für Frage</div>
                     </div>
                 </div>
             </div>
